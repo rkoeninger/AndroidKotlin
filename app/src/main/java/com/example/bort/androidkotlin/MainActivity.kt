4afb27e98ee0fbb666694e7c347c3e3920a67953
rkoeninger <rkoeninger@att.net>
2017-01-01 02:28:14 -0500

    Added some camera light control code

--- app/src/main/java/com/example/bort/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bort/androidkotlin/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.bort.androidkotlin
 
+import android.content.pm.PackageManager
+import android.hardware.Camera
+import android.hardware.Camera.Parameters
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -8,6 +11,8 @@ import android.support.v7.widget.Toolbar
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,5 +47,41 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    var camRef: MutableList<Camera> = mutableListOf()
+    var flashMode = Parameters.FLASH_MODE_AUTO
+
+    fun onLightToggle(view: View) {
+        val butt = findViewById(R.id.toggleButton) as Button
+        butt.text = if (butt.text == "On") "Off" else "On"
+        val tv = findViewById(R.id.textView) as TextView
+        val flashAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+        tv.text = if (flashAvailable) "Flash Available" else "FLASH NOT FOUND"
+        if (flashAvailable) {
+            val cam = if (camRef.size > 0) {
+                camRef[0]
+            } else {
+                val c = Camera.open()
+                camRef.add(c)
+                c
+            }
+            if (cam == null) {
+                tv.text = "No Camera"
+                return
+            }
+            val p = cam.parameters
+            if (p.flashMode == Parameters.FLASH_MODE_TORCH) {
+                p.flashMode = Parameters.FLASH_MODE_AUTO
+                cam.parameters = p
+                cam.stopPreview()
+                tv.text = "Light Turned Off"
+            } else {
+                p.flashMode = Parameters.FLASH_MODE_TORCH
+                cam.parameters = p
+                cam.startPreview()
+                tv.text = "Light Turned On"
+            }
+        }
     }
 }
