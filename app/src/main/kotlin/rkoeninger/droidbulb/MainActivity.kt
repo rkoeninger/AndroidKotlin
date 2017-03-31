2d37107cfc1ac73fb5ae1ce6801dd2e6769a2693
Robert Koeninger <rkoeninger@att.net>
2017-03-31 00:18:29 -0400

    Simplified UI, used vector icon on image button

--- app/src/main/kotlin/rkoeninger/droidbulb/MainActivity.kt
+++ app/src/main/kotlin/rkoeninger/droidbulb/MainActivity.kt
@@ -1,77 +1,54 @@
+@file:Suppress("DEPRECATION")
+
 package rkoeninger.droidbulb
 
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.hardware.Camera
 import android.hardware.Camera.Parameters
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.View
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    var camRef: Camera? = null
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        if (id == R.id.action_settings) {
-            return true
+    override fun onDestroy() {
+        super.onDestroy()
+        camRef?.let {
+            it.parameters.flashMode = Parameters.FLASH_MODE_AUTO
+            it.stopPreview()
         }
-
-        return super.onOptionsItemSelected(item)
+        camRef = null
     }
-
-    var camRef: MutableList<Camera> = mutableListOf()
-    var flashMode = Parameters.FLASH_MODE_AUTO
 
     fun onLightToggle(@Suppress("UNUSED_PARAMETER") view: View) {
-        val butt = findViewById(R.id.toggleButton) as Button
-        butt.text = if (butt.text == "On") "Off" else "On"
-        val flashAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+        val button = findViewById(R.id.toggleButton) as ImageButton
 
-        if (flashAvailable) {
-            val cam =
-                if (camRef.size > 0) {
-                    camRef[0]
-                } else {
-                    val c = Camera.open()
-                    camRef.add(c)
-                    c
-                }
-
-            if (cam == null) {
-                return
-            }
-
-            val p = cam.parameters
-
-            if (p.flashMode == Parameters.FLASH_MODE_TORCH) {
-                p.flashMode = Parameters.FLASH_MODE_AUTO
-                cam.parameters = p
-                cam.stopPreview()
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            if (camRef == null) {
+                val camera = Camera.open()
+                camera.parameters.flashMode = Parameters.FLASH_MODE_TORCH
+                camera.startPreview()
+                camRef = camera
+                setButtonIcon(true, button)
             } else {
-                p.flashMode = Parameters.FLASH_MODE_TORCH
-                cam.parameters = p
-                cam.startPreview()
+                camRef?.parameters?.flashMode = Parameters.FLASH_MODE_AUTO
+                camRef?.stopPreview()
+                camRef = null
+                setButtonIcon(false, button)
             }
         }
+    }
+
+    fun setButtonIcon(on: Boolean, button: ImageButton) {
+        val iconId = if (on) R.drawable.ic_droidbulb else R.drawable.ic_droidbulboff
+        button.setImageDrawable(resources.getDrawable(iconId))
     }
 }
